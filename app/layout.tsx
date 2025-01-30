57637ce46cd73c9ab92d698bf33cd5138832cc41
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-01-30 11:00:53 +0800

    update: import next themes for them switching

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const inter = Inter({
     variable: "--font-inter",
@@ -19,11 +20,18 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <body
                 className={`${inter.variable} antialiased`}
             >
-                {children}
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="dark"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
             </body>
         </html>
     );
