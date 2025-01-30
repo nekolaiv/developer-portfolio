fee03ee80257aea12eae8a05db0c9a60a3b887e9
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-01-30 10:30:55 +0800

    update: add data file

---
+++ data/index.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+  { name: "Hero", link: "#hero" },
+  { name: "Stacks", link: "#stacks" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
