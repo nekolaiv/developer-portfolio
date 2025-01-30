3d4ea01777873add243db0ac81834f5d5bf9e1dd
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-01-30 10:33:25 +0800

    update: add tw config codes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from "tailwindcss";
+
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 export default {
     content: [
@@ -14,5 +20,20 @@ export default {
             },
         },
     },
-    plugins: [],
+    darkMode: "class",
+    plugins: [
+        addVariablesForColors,
+    ],
 } satisfies Config;
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
