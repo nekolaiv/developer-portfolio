dedda7b673f65436572c2d0b90678fb95ea93555
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-01-31 08:46:48 +0800

    update: set temporary screen sizes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { FloatingNav } from "@/components/FloatingNavBar";
+import { navItems } from "@/data";
+
 export default function Home() {
     return (
-        <div className="w-screen h-screen flex justify-center items-center border border-red-700">
-            <h1 className="text-9xl">Hello World</h1>
-        </div>
+        <main className="w-full min-h-[200vh] flex flex-col border border-red-700">
+			<div className="flex flex-col flex-1 justify-center items-center">
+				<FloatingNav navItems={navItems} />
+				<h1 className="text-4xl md:text-6xl lg:text-9xl">Hello World</h1>
+			</div>
+		</main>
     );
 }
